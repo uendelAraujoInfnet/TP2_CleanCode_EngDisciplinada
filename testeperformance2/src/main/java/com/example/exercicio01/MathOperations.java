9a6a163b319b1454c144c8de509d96fb7091d9bd
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2025-03-03 08:57:28 -0500

    feat - first question about TP2

---
+++ testeperformance2/src/main/java/com/example/exercicio01/MathOperations.java
@@ -0,0 +1,8 @@
+package com.example.exercicio01;
+
+public class MathOperations {
+  public static int doubleSum(int firstNumber, int secondNumber){
+    int sum = firstNumber + secondNumber;
+    return sum * 2;
+  }
+}
